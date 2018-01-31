cd7a59d563076c1b43c6a661ac239c03ed6509b0
reinhard-ulrich.herrmann@web.de <ErikSaeger01>
2018-01-31 08:01:56 +0100

    Initial commit

---
+++ app/src/main/java/de/orome/todoliste/Activity_TodoUebersicht.java
@@ -0,0 +1,14 @@
+package de.orome.todoliste;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_TodoUebersicht extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_uebersicht);
+    }
+
+}
